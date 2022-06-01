b946fff31c9e16901999fc5bc9fbde437b75475c
Vgyand <gyandzhunczevv@gmail.com>
2022-06-01 20:33:19 +0300

    agregation

--- back/src/actor/actor.service.ts
+++ back/src/actor/actor.service.ts
@@ -33,10 +33,21 @@ export class ActorService {
 
         //agreg
 
-        return this.ActorModel
-            .find(options)
-            .select('-updatedAt -__v')
-            .sort({ createdAt: 'desc' })
+        return this.ActorModel.aggregate().match(options).lookup({
+            from: 'Movie',
+            foreignField: 'actors',
+            localField: '_id',
+            as: 'movies'
+        }).addFields({
+            countMovies: {
+                $size: '$movies'
+            }
+        }).project({
+            __v: 0,
+            updatedAt: 0,
+            movies: 0
+        })
+            .sort({ createdAt: -1 })
             .exec()
     }
 
