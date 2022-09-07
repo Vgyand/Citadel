b603888cd1f83212993babc22926ca27c1142213
Vgyand <gyandzhunczevv@gmail.com>
2022-09-07 18:29:46 +0300

    HeadingComponent

--- front/app/components/screens/home/Home.tsx
+++ front/app/components/screens/home/Home.tsx
@@ -2,13 +2,12 @@
 import Meta from "@/utils/meta/Meta"
 import { FC } from "react"
 import { IHome } from "./home.interface"
-import logoImage from '../../../assets/images/heart.svg'
-import Image from "next/image"
+import Heading from "@/components/ui/heading/Heading"
 
 const Home: FC<IHome> = () => {
     return (
         <Meta title='Citadel' description='online cinema'>
-            <h1>Home</h1>
+            <Heading title="Home page" />
         </Meta>
     )
 }
