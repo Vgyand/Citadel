f0b74797eeb1024ebb715010f26d6fcb1238816e
Vgyand <gyandzhunczevv@gmail.com>
2022-09-09 20:03:45 +0300

    urlFIx

--- front/app/components/layout/Navigation/Logo.tsx
+++ front/app/components/layout/Navigation/Logo.tsx
@@ -5,7 +5,7 @@ import logo from '@/assets/images/heart.svg'
 
 const Logo: FC = () => {
     return (
-        <Link href="">
+        <Link href="/">
             <a className='px-layout mb-10 block'>
                 <Image src={logo} width={54} height={54} alt={'Logo'} draggable={false} /> <span className='text-white text-3xl'>Citadel</span>
             </a>
