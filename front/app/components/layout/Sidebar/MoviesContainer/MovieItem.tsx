3c69f8a84915907da85d255d404b91409e518924
Vgyand <gyandzhunczevv@gmail.com>
2022-09-09 19:26:37 +0300

    404/correctUrls

--- front/app/components/layout/Sidebar/MoviesContainer/MovieItem.tsx
+++ front/app/components/layout/Sidebar/MoviesContainer/MovieItem.tsx
@@ -1,6 +1,8 @@
 import MaterialIcon from '@/components/ui/MaterialIcon'
 import { getMoviesUrl } from '@/config/api.config'
+import { getGenreUrl } from '@/config/url.config'
 import { IMovie } from '@/shared/types/movie.types'
+import { getGenresListEach } from '@/utils/movie/getGenresList'
 import Image from 'next/image'
 import Link from 'next/link'
 import { FC } from 'react'
@@ -15,7 +17,7 @@ import styles from './MovieList.module.scss'
 const MovieItem: FC<{ movie: IMovie }> = ({ movie }) => {
     return (
         <div className={styles.item}>
-            <Link href={getMoviesUrl(movie.slug)}>
+            <Link href={getMoviesUrl(`/${movie.slug}`)}>
                 <a>
                     <Image
                         alt={movie.title}
@@ -31,6 +33,11 @@ const MovieItem: FC<{ movie: IMovie }> = ({ movie }) => {
                 <div>
                     <div className={styles.title}>{movie.title}</div>
                     <div className={styles.genres}>
+                        {movie.genres.map(({ slug, name, _id }, idx) => (
+                            <Link key={_id} href={getGenreUrl(`/${slug}`)}>
+                                <a>{getGenresListEach(idx, movie.genres.length, name)}</a>
+                            </Link>
+                        ))}
                     </div>
                 </div>
                 <div className={styles.rating}>
